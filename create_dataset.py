24c0e441e1f6168cfb0c2d432d2df7dc817e3e76
Banghao <banghao_chi@163.com>
2024-10-08 11:15:10 -0500

    feat: datasets for prof and movie

--- create_dataset.py
+++ create_dataset.py
@@ -1,28 +1,20 @@
 from firecrawl import FirecrawlApp
 import os
 
+entity = "movie"
 fire_app = FirecrawlApp(api_key=os.getenv("FIRECRAWL_API_KEY"))
+source_path = "dataset/source"
 
 def create_dataset(url: str, file_name: int):
     scrape_result = fire_app.scrape_url(
-        url, params={"formats": ["markdown"], "excludeTags": ["a", "img", "video"]}
+        url, params={"formats": ["markdown"], "excludeTags": ["img", "video"]}
     )["markdown"]
-    with open(f"dataset/article/{file_name}.txt", "w") as f:
+    with open(f"dataset/article/{entity}/{file_name}.txt", "w") as f:
         f.write(scrape_result)
 
 
 if __name__ == "__main__":
-    urls = [
-        "https://www.caranddriver.com/reviews/a62019773/2025-volvo-ex90-drive/",
-        "https://www.caranddriver.com/reviews/a62043196/2025-hyundai-tucson-hybrid-drive/",
-        "https://www.caranddriver.com/reviews/a62017885/2025-volkswagen-jetta-drive/",
-        "https://www.caranddriver.com/reviews/a62021190/2025-volkswagen-jetta-gli-test/",
-        "https://www.caranddriver.com/reviews/a60777159/2025-genesis-gv80-suv-drive/",
-        "https://www.caranddriver.com/reviews/a61828804/2024-mercedes-amg-gt63-coupe-test/",
-        "https://www.caranddriver.com/reviews/a61689886/2025-lexus-ux300h-hybrid-test/",
-        "https://www.caranddriver.com/reviews/a61557962/2024-toyota-land-cruiser-first-edition-test/",
-        "https://www.caranddriver.com/reviews/a61069740/2025-honda-civic-hybrid-prototype-drive/",
-        "https://www.caranddriver.com/reviews/a60499379/2025-toyota-camry-drive/"
-    ]
+    with open(source_path + "/" + entity + ".txt", "r") as f:
+        urls = f.readlines()
     for i, url in enumerate(urls):
         create_dataset(url, i)
