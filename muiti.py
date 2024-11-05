0c265718575135dcf551c5e1c81013b4efc00e54
Banghao <banghao_chi@163.com>
2024-11-05 13:38:21 -0600

    feat: raw extract and multi-page

---
+++ muiti.py
@@ -0,0 +1,59 @@
+from typing import Annotated, List
+import os
+
+from firecrawl import FirecrawlApp
+from pydantic import BaseModel, BeforeValidator, HttpUrl, TypeAdapter
+from openai import OpenAI
+import instructor
+
+fire_app = FirecrawlApp(api_key=os.getenv("FIRECRAWL_API_KEY"))
+
+with open("./dataset/article/prof/0.txt", "r") as file:
+    scrape_result = file.read()
+
+open_source_model = "Qwen/Qwen2.5-72B-Instruct-AWQ"
+prompt_type = "prof"
+
+http_url_adapter = TypeAdapter(HttpUrl)
+
+Url = Annotated[
+    str, BeforeValidator(lambda value: str(http_url_adapter.validate_python(value)))
+]
+
+
+class RelatedLinks(BaseModel):
+    related_links: List[Url]
+
+
+def get_links_from_init_page(scrape_result):
+    # client = OpenAI(base_url="http://localhost:8888/v1")
+    # client = OpenAI(base_url="http://Osprey1.csl.illinois.edu:8000/v1")
+    client = OpenAI(base_url="http://Osprey2.csl.illinois.edu:8000/v1")
+    client = instructor.from_openai(client)
+    response = client.chat.completions.create(
+        model=open_source_model,
+        messages=[
+            {
+                "role": "system",
+                "content": f"You are an expert at summarizing {prompt_type} articles in JSON format. Now you are given the initial page of the {prompt_type} article, please extract the related links that may include information about the {prompt_type} from the page.",
+            },
+            {
+                "role": "user",
+                "content": f"The initial page of the {prompt_type} is: " + scrape_result,
+            },
+        ],
+        max_tokens=16384,
+        temperature=0.0,
+        response_model=RelatedLinks,
+    )
+    return response.related_links
+
+
+links = get_links_from_init_page(scrape_result)
+print(links)
+
+for i, link in enumerate(links):
+    scrape_result = scrape_result + fire_app.scrape_url(link, params={"formats": ["markdown"], "excludeTags": ["img", "video"]})["markdown"]
+
+# with open(f"./dataset/article/{prompt_type}/0.txt", "w") as f:
+#     f.write(scrape_result)
